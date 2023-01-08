055b79d59c6743fe6ffbd3abfaae0134b9c456d1
shashikant88093 <shashi94goswami@gmail.com>
2023-01-08 23:20:12 +0530

    :golata

--- index.js
+++ index.js
@@ -1,51 +1,86 @@
 //jshint esversion:6
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
+});
+//app
+const app = express();
+//body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+//
+//schema
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please check your data entry, no name specified!"],
+  },
+  age: {
+    type: Number
+  },
+});
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+//model
+const person = mongoose.model("person", personSchema);
 
-// Database Name
-const dbName = 'fruitsDB';
+//create new fruit
+const persons = new person({
+  name: "Shashikant",
+  age: 25,
+});
 
-// Create a new MongoClient
-const client = new MongoClient(url,{useNewUrlParser:true});
+const personone = new person({
+  name: "chandan",
+  age: 25,
+});
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+const persontwo = new person({
+  name: "raunk",
+  age: 25,
+});
 
-  const db = client.db(dbName);
-  insertDocuments(db, function() {
-    findDocuments(db, function() {
-      client.close();
+person.insertMany([persons,personone, persontwo], function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved all the person to fruitsDB");
+  }
+});
+
+person.find(function (err, persons) {
+  if (err) {
+    console.log(err);
+  } else {
+    persons.forEach(function (person) {
+      console.log(person.name);
     });
-})
+    mongoose.connection.close();
+  }
 
 });
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-      {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3,result.insertedCount);
-      assert.equal(3,Object.keys(result.insertedIds).length);
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    });
+
+//save fruit
+persons.save();
+
+//update person
+person.updateOne({ _id: "63baf3e7cdf22cecc2186832" }, { name: "Shashi",age:20 }, function (
+  err
+) {
+  if (err) {
+    console.log(err,"error in update");
+  } else {
+    console.log("Successfully updated the document.");
   }
-  const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-      assert.equal(err, null);
-      console.log("Found the following records");
-      console.log(docs)
-      callback(docs);
-    });
+});
+
+person.deleteOne({ _id: "63baf3e7cdf22cecc2186832" }, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully deleted the document.");
   }
+  
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
